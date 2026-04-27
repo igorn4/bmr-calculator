7c14d321932d297dba29687076f79e476e607211
IGOR NEVES DA SILVA <E807125@copel.com>
2026-04-27 14:20:50 -0300

    ^Q

--- bmrCalculator/calculator/models.py
+++ bmrCalculator/calculator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    age = models.IntegerField()
+    birth_date = models.DateField()
+    height = models.FloatField()
+    sex = models.BooleanField()
+
+class DailyRegister(models.Model):
+    weight = models.FloatField()
+    calories_consumed = models.FloatField()
