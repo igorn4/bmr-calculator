a18443c047ea5c946edc37dfe7e9a95d4889e2cf
IGOR NEVES DA SILVA <E807125@copel.com>
2026-05-11 09:56:15 -0300

    renamed some files and created new folders

--- bmrCalculator/calculator/models.py
+++ bmrCalculator/calculator/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class UserProfile(models.Model):
     age = models.IntegerField()
     height = models.IntegerField()
--- bmrCalculator/calculator/views.py
+++ bmrCalculator/calculator/views.py
@@ -1,3 +1,5 @@
+from urllib import request
+
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,9 +14,10 @@ class TMBCalculus(APIView): # this class is called when we access the endpoint /
 
     def get(self, request):
         registros = UserProfile.objects.all().order_by('-id')
-        return render(request, 'calculatorMain/index.html', {'registros': registros})
+        return render(request, 'calculatorMain/datafilling.html', {'registros': registros})
 
     def post(self, request):
+
         serializer = UserProfileSerializer(data=request.data)
 
         if serializer.is_valid():
